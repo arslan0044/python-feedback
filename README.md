1a09abf41a17b9ec68596cc71f1d68059575359b
bounce-dev <bouncebusinessro@gmail.com>
2020-09-29 18:51:31 +0300

    added float layout
    going to work on it later this day

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # python-feedback
- An app made in Kivy that is compatible on most devices, that stores feedback given by the user into a mysql database. My first project in Python.
+ An app made in Python + Kivy that is compatible on most devices, that stores feedback given by the user into a MYSQL database. My first project in Python.
--- main.py
+++ main.py
@@ -1,32 +1,15 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
 
 
-class MyGrid(Widget):
-    #some widgets added here
-    name = ObjectProperty(None)
-    experience = ObjectProperty(None)
-    stars = ObjectProperty(None)
-
-    def submit_btn(self):
-        print("Name: ", self.name.text, "Your Experience: ", self.experience.text, "Stars: ", self.stars.text)
-
-        #empty strings after clicking submit
-        self.name.text = ""
-        self.experience.text = ""
-        self.stars.text = ""
 
 
-#just dont touch this
+
+#just dont touch this unless you know what you are doing
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        return FloatLayout()
 
 if __name__ == "__main__":
     MyApp().run()
