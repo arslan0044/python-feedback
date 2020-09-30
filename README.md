19c77e2c84554a349e792053ead0e46c1d7438b4
bounce-dev <bouncebusinessro@gmail.com>
2020-10-01 00:12:30 +0300

    modified minor stuff

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
  An app made in Python + Kivy that is compatible on most devices and stores feedback given by the user into a MYSQL database. My first project in Python.
 
  Things to add:\
-  Labels - Name, Experience, Stars\
+  Labels - Name, Experience, Stars (added)\
   Drop Down Menus - Name, Experience\
-  Buttons - Stars (1-5), Submit Button
+  Buttons - Stars (1-5), Submit Button (added)
+  Front End - Better Design (ill add it after backend is done)
--- main.py
+++ main.py
@@ -6,11 +6,20 @@ from kivy.uix.button import Button
 from kivy.base import runTouchApp
 from kivy.uix.behaviors import ToggleButtonBehavior
 from kivy.uix.togglebutton import ToggleButton
+from kivy.core.text import LabelBase
 
 Window.clearcolor = (1, 1, 1, 1) #white background
 
+LabelBase.register(name="KeepCalm", #adding the font KeepCalm
+    fn_regular= "KeepCalm-Medium.ttf"
+    )
+
+
 #just dont touch this unless you know what you are doing
 class MyApp(App):
+    def test69(self):
+        print("a mers baa")
+
     def build(self):
         return FloatLayout()
 
