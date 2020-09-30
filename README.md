ed4ca449cf8369f3fa577e2ef68cfdfc5542289e
bounce-dev <bouncebusinessro@gmail.com>
2020-10-01 00:50:05 +0300

    changed the system
    decided to change the employee list from a drop down list to a new activity with buttons, added debug "prints" to the stars buttons

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  An app made in Python + Kivy that is compatible on most devices and stores feedback given by the user into a MYSQL database. My first project in Python.
 
  Things to add:\
-  Labels - Name, Experience, Stars (added)\
-  Drop Down Menus - Name, Experience\
-  Buttons - Stars (1-5), Submit Button (added)
+  Labels - Name, Stars (added)\
+  A new activity with employees names (buttons) that will redirect to main activity on touch\
+  Buttons - Stars (1-5), Submit Button (added)\
   Front End - Better Design (ill add it after backend is done)
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from kivy.base import runTouchApp
 from kivy.uix.behaviors import ToggleButtonBehavior
 from kivy.uix.togglebutton import ToggleButton
 from kivy.core.text import LabelBase
+from kivy.uix.dropdown import DropDown
+from kivy.uix.slider import Slider
 
 Window.clearcolor = (1, 1, 1, 1) #white background
 
@@ -17,8 +19,20 @@ LabelBase.register(name="KeepCalm", #adding the font KeepCalm
 
 #just dont touch this unless you know what you are doing
 class MyApp(App):
-    def test69(self):
-        print("a mers baa")
+    def star1(self):
+        print("it worked - star 1")
+
+    def star2(self):
+        print("it worked - star 2")
+
+    def star3(self):
+        print("it worked - star 3")
+
+    def star4(self):
+        print("it worked - star 4")
+
+    def star5(self):
+        print("it worked - star 5")
 
     def build(self):
         return FloatLayout()
