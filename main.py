52d417b5c23d96e7792baaf38cd9b53ac0238508
bounce-dev <bouncebusinessro@gmail.com>
2020-09-30 21:11:24 +0300

    added a lot of things
    changed some sizes, added white background, added more buttons and labels. Also added 2 images for future use

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import kivy
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+from kivy.uix.button import Button
+from kivy.base import runTouchApp
+from kivy.uix.behaviors import ToggleButtonBehavior
+from kivy.uix.togglebutton import ToggleButton
 
-
-
-
+Window.clearcolor = (1, 1, 1, 1) #white background
 
 #just dont touch this unless you know what you are doing
 class MyApp(App):
