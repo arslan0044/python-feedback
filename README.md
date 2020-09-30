f5489e5161768b283bc83e3a98049d579ceb2d18
bounce-dev <bouncebusinessro@gmail.com>
2020-10-01 01:27:23 +0300

    added screenmanager and more
    added screen manager (the ability to switch to mainwindow to secondwindow), changed the resolution of the window

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 
  Things to add:\
   Labels - Name, Stars (added)\
-  A new activity with employees names (buttons) that will redirect to main activity on touch\
-  Buttons - Stars (1-5), Submit Button (added)\
+  A new activity with employees names (buttons) that will redirect to main activity on touch (added)\
+  Buttons - Stars (1-5) - (added), Submit Button, Employees Names Buttons\
   Front End - Better Design (ill add it after backend is done)
--- main.py
+++ main.py
@@ -9,16 +9,28 @@ from kivy.uix.togglebutton import ToggleButton
 from kivy.core.text import LabelBase
 from kivy.uix.dropdown import DropDown
 from kivy.uix.slider import Slider
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-Window.clearcolor = (1, 1, 1, 1) #white background
+class MainWindow(Screen):
+    pass
+
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("my.kv") #this basically defines the kv file that we are using for this screen
 
 LabelBase.register(name="KeepCalm", #adding the font KeepCalm
     fn_regular= "KeepCalm-Medium.ttf"
     )
-
+Window.clearcolor = (1, 1, 1, 1)
+Window.size = (1280, 800) #this is usually a resolution used by tablets (the main device i want to use this one)
 
 #just dont touch this unless you know what you are doing
-class MyApp(App):
+class MyMainApp(App):
     def star1(self):
         print("it worked - star 1")
 
@@ -34,8 +46,9 @@ class MyApp(App):
     def star5(self):
         print("it worked - star 5")
 
+
     def build(self):
-        return FloatLayout()
+        return kv
 
 if __name__ == "__main__":
-    MyApp().run()
+    MyMainApp().run()
