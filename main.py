13d3752636a4c550de29e6a5d7986ef0c0e1099f
bounce-dev <bouncebusinessro@gmail.com>
2020-10-01 11:23:17 +0300

    added mysql support
    added mysql support, still working on a more efficient system tho

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ from kivy.uix.dropdown import DropDown
 from kivy.uix.slider import Slider
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="isaccea"
+)
+
+mycursor = mydb.cursor()
 
 class MainWindow(Screen):
     pass
@@ -29,6 +39,13 @@ LabelBase.register(name="KeepCalm", #adding the font KeepCalm
 Window.clearcolor = (1, 1, 1, 1)
 Window.size = (1280, 800) #this is usually a resolution used by tablets (the main device i want to use this one)
 
+#global vars
+employee1_chosed = False
+employee2_chosed = False
+employee3_chosed = False
+employee4_chosed = False
+employee5_chosed = False
+employee6_chosed = False
 #just dont touch this unless you know what you are doing
 class MyMainApp(App):
     def star1(self):
@@ -46,6 +63,90 @@ class MyMainApp(App):
     def star5(self):
         print("it worked - star 5")
 
+        #this is probably the most inefficient code i ever wrote
+
+    employee1 = "Michael"
+    employee2 = "John"
+    employee3 = "Betty"
+    employee4 = "Emily"
+    employee5 = "Elizabeth"
+    employee6 = "George"
+
+    def employee1_choose(self):
+        global employee1_chosed
+        employee1_chosed = True
+        #settings these all to false to only 1 at the time can be chosen
+        employee2_chosed = False
+        employee3_chosed = False
+        employee4_chosed = False
+        employee5_chosed = False
+        employee6_chosed = False
+        print("employee1_choose was set to: " + str(employee1_chosed))
+
+        if employee1_chosed == True:
+            print("trying step 2")
+
+            sql = "INSERT INTO feedback (name, stars) VALUES (%s, %s)"
+            val = ("Michael", "")
+            mycursor.execute(sql, val)
+
+            mydb.commit()
+            print(mycursor.rowcount, "record inserted. step 2 worked")
+
+    def employee2_choose(self):
+        global employee2_chosed
+        #settings these all to false to only 1 at the time can be chosen
+        employee1_chosed = False
+        employee2_chosed = True
+        employee3_chosed = False
+        employee4_chosed = False
+        employee5_chosed = False
+        employee6_chosed = False
+        print("employee2_choose was set to: " + str(employee2_chosed))
+
+    def employee3_choose(self):
+        global employee3_chosed
+        #settings these all to false to only 1 at the time can be chosen
+        employee1_chosed = False
+        employee2_chosed = False
+        employee3_chosed = True
+        employee4_chosed = False
+        employee5_chosed = False
+        employee6_chosed = False
+        print("employee3_choose was set to: " + str(employee3_chosed))
+
+    def employee4_choose(self):
+        global employee4_chosed
+        #settings these all to false to only 1 at the time can be chosen
+        employee1_chosed = False
+        employee2_chosed = False
+        employee3_chosed = False
+        employee4_chosed = True
+        employee5_chosed = False
+        employee6_chosed = False
+        print("employee4_choose was set to: " + str(employee4_chosed))
+
+    def employee5_choose(self):
+        global employee5_chosed
+        #settings these all to false to only 1 at the time can be chosen
+        employee1_chosed = False
+        employee2_chosed = False
+        employee3_chosed = False
+        employee4_chosed = False
+        employee5_chosed = True
+        employee6_chosed = False
+        print("employee5_choose was set to: " + str(employee5_chosed))
+
+    def employee6_choose(self):
+        global employee6_chosed
+        #settings these all to false to only 1 at the time can be chosen
+        employee1_chosed = False
+        employee2_chosed = False
+        employee3_chosed = False
+        employee4_chosed = False
+        employee5_chosed = False
+        employee6_chosed = True
+        print("employee6_choose was set to: " + str(employee6_chosed))
 
     def build(self):
         return kv
